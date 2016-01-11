b07e8b079f2fe586a34fc6d3f1460acf1d800c10
Erik Allik <eallik@gmail.com>
2016-01-11 13:52:05 +0200

    even deeper emulation + more coming

--- LazyEvaluator.java
+++ LazyEvaluator.java
@@ -23,12 +23,20 @@ class Thunk<A> {
 
   public interface Compute<A> { A compute(Void v); }
   public static <A> Thunk<A> make(final Compute<A> compute) {
-    return new Thunk<A>() { public A compute() { return compute.compute(null); } };
+    return new Thunk<A>() { public A compute() {
+      return compute.compute(null);
+    } };
   }
 }
 
 interface Fn<ArgT, RetT> {
   Thunk<RetT> call(final Thunk<ArgT> arg);
+
+  public static <ArgT, RetT>
+    Thunk<RetT> apply(Thunk<Fn<ArgT, RetT>> f, Thunk<ArgT> x)
+  {
+    return f.eval().call(x);
+  }
 }
 
 /** Linked List; the Haskell
@@ -36,71 +44,73 @@ interface Fn<ArgT, RetT> {
  * represented by internal state
  */
 class LList<A> {
-  public final Thunk<A>       head;
+  public final Thunk<      A > head;
   public final Thunk<LList<A>> tail;
   LList(Thunk<A> head, Thunk<LList<A>> tail) {
     this.head = head;  this.tail = tail;
   }
 
-  public final boolean isNil()  { return head == null; }
-  public final boolean isCons() { return head != null; }
-
   // this completely bypasses the evaluation engine,
   // but for the sake of simplicity, let's not take the step
   // to lazy strings and such.
   public String toString() {
-    return isNil()
+    return LList.isNil(Thunk.ready(this)).eval()
       ? "[]"
       : head.toString() + " : " + tail.toString();
   }
 
+  public static <A> Thunk<A> head(Thunk<LList<A>> xs) { return Thunk.make(__ -> {
+    return xs.eval().head.eval();
+  }); }
+  public static <A> Thunk<LList<A>> tail(Thunk<LList<A>> xs) { return Thunk.make(__ -> {
+    return xs.eval().tail.eval();
+  }); }
   public static <A> Thunk<LList<A>> nil() {
     return Thunk.ready(new LList<A>(null, null));
   }
   public static <A> Thunk<LList<A>> cons(final Thunk<A> head, final Thunk<LList<A>> tail) {
     return Thunk.make(__ -> new LList<A>(head, tail));
   }
+  public static <A> Thunk<Boolean> isNil(final Thunk<LList<A>> xs) {
+    return Thunk.make(__ -> xs.eval().head == null);
+  }
+  public static <A> Thunk<Boolean> isCons(final Thunk<LList<A>> xs) {
+    return Thunk.make(__ -> xs.eval().head != null);
+  }
 }
 
+// interface Fn2<A, B, R> extends Fn<A, Fn<B, R>> {}
+// interface Fn3<A, B, C, R> extends Fn<A, Fn<B, Fn<C, R>>> {}
+
 public class LazyEvaluator {
-  static Fn<Double, Double> incrByD(final double d) {
-    return arg -> Thunk.ready(arg.eval() + d);
-  }
-  static Fn<Double, Double> mulByD(final double factor) {
-    return arg -> Thunk.ready(arg.eval() * factor);
-  }
-  static Fn<Integer, Integer> incrByI(final int d) {
-    return arg ->Thunk.ready(arg.eval() + d);
-  }
-  static Fn<Integer, Integer> mulByI(final int factor) {
-    return arg -> Thunk.ready(arg.eval() * factor);
-  }
+  static <A> Thunk<A> if_(Thunk<Boolean> cond,
+                          Thunk<A> v1,
+                          Thunk<A> v2) { return Thunk.make(__ -> {
+    return (cond.eval() ? v1 : v2).eval();
+  }); }
+
+  static Fn<Double, Double> incrByD(final double d) { return arg -> Thunk.ready(arg.eval() + d); }
+  static Fn<Double, Double> mulByD(final double factor) { return arg -> Thunk.ready(arg.eval() * factor); }
+  static Fn<Integer, Integer> incrByI(final int d) { return arg ->Thunk.ready(arg.eval() + d); }
+  static Thunk<Fn<Integer, Integer>> mulByI(final int factor) { return Thunk.ready(arg -> Thunk.ready(arg.eval() * factor)); }
 
   public static void main(String[] args) {
-    // Thunk<LList<Double>> nums1 = generate(Thunk.ready(0.0), incrByD(1.0));
-    // Thunk<LList<Double>> nums2 = map(mulByD(1.0), nums1);
-    // Thunk<LList<Double>> nums2first4 = take(Thunk.ready(4), nums2);
-    // Thunk<LList<Double>> nums2first5 = take(Thunk.ready(5), nums2);
-    // System.out.println("-----");
-
-    // // printList(nums2first4);
-    // printList(nums2first4);
-    // printList(nums2first5);
-    // System.out.println(sum(nums2first4));
-    // System.out.println(len(nums2first4));
-    // System.out.println(avg(nums2first4));
-
-    /////////////////////////////////
-
-    Fn<Integer, Boolean> isEven = x -> even(x);
-
     Thunk<LList<Integer>> nums = generate(Thunk.ready(0), incrByI(1));
+    Thunk<LList<Integer>> dblNums = map(mulByI(2), nums);
+
+    Thunk<Fn<Integer, Boolean>> isEven = Thunk.ready(x -> even(x));
     Thunk<LList<Integer>> evens = filter(isEven, nums);
     System.out.println(take(Thunk.ready(3), evens));
 
     Thunk<LList<Integer>> primes_ = primes();
     Thunk<LList<Integer>> doublePrimes = map(mulByI(2), primes_);
     printList(take(Thunk.ready(10), doublePrimes));
+
+    /////////////////////
+
+    Thunk<Integer> value =
+      if_(Thunk.ready(true), Thunk.ready(3), Thunk.ready(4));
+    System.out.println(value);
   };
 
   static Thunk<Boolean> even(final Thunk<Integer> i) {
@@ -111,17 +121,17 @@ public class LazyEvaluator {
     return a.eval() == b.eval();
   }); }
 
-  static Thunk<Double> sum(final Thunk<LList<Double>> xs) { return Thunk.make(__ -> {
-    return xs.eval().isNil()
-      ? 0.0
-      : xs.eval().head.eval() + sum(xs.eval().tail).eval();
-  }); }
+  static Thunk<Double> sum(final Thunk<LList<Double>> xs) {
+    return if_(LList.isNil(xs),
+               Thunk.ready(0.0),
+               Thunk.make(__ -> xs.eval().head.eval() + sum(xs.eval().tail).eval()));
+  }
 
-  static <A> Thunk<Integer> len(final Thunk<LList<A>> xs) { return Thunk.make(__ -> {
-    return xs.eval().isNil()
-      ? 0
-      : 1 + len(xs.eval().tail).eval();
-  }); }
+  static <A> Thunk<Integer> len(final Thunk<LList<A>> xs) {
+    return if_(LList.isNil(xs),
+               Thunk.ready(0),
+               Thunk.make(__ -> 1 + len(xs.eval().tail).eval()));
+  }
 
   static Thunk<Double> avg(final Thunk<LList<Double>> xs) { return Thunk.make(__ ->  {
     final Integer n = len(xs).eval();
@@ -142,29 +152,29 @@ public class LazyEvaluator {
       : new LList<A>(xs.eval().head, take(Thunk.ready(n.eval() - 1), xs.eval().tail));
   }); }
 
-  static <A, B> Thunk<LList<B>> map(final Fn<A, B> f, final Thunk<LList<A>> xs) { return Thunk.make(__ -> {
-    return xs.eval().isNil()
-      ? new LList<B>(null, null)
-      : new LList<B>(f.call(xs.eval().head),
-                    map(f, xs.eval().tail));
-  }); }
+  static <A, B> Thunk<LList<B>> map(final Thunk<Fn<A, B>> f, final Thunk<LList<A>> xs) {
+    return if_(LList.isNil(xs),
+               LList.<B>nil(),
+               LList.<B>cons(Fn.apply(f, LList.head(xs)),
+                             Thunk.make(__ -> map(f, LList.tail(xs)).eval())));
+  }
 
-  static <A> Thunk<LList<A>> filter(final Fn<A, Boolean> pred,
-                                    final Thunk<LList<A>> xs) { return Thunk.make(__ -> {
-    if (xs.eval().isNil()) {
-      return xs.eval();
-    } else {
-      Thunk<A> head        = xs.eval().head;
-      Thunk<LList<A>> tail = xs.eval().tail;
-      if (pred.call(head).eval())
-        return LList.cons(head, filter(pred, tail)).eval();
-      else
-        return                  filter(pred, tail) .eval();
-    }
-  }); }
+  static <A> Thunk<LList<A>> filter(final Thunk<Fn<A, Boolean>> pred,
+                                    final Thunk<LList<A>> xs) {
+        return if_(LList.isNil(xs),
+                   xs,
+                   Thunk.make(___ -> {
+                       Thunk<A> head        = LList.head(xs);
+                       Thunk<LList<A>> tail = LList.tail(xs);
+                       return if_(Fn.apply(pred, head),
+                                  LList.cons(head, filter(pred, tail)),
+                                                   filter(pred, tail)).eval();
+                     })
+                   );
+  }
 
   static <A> void printList(final Thunk<LList<A>> xs) {
-    if (xs.eval().isNil())
+    if (LList.isNil(xs).eval())
       System.out.println("[]");
     else {
       final String str = xs.eval().head.toString();
@@ -185,7 +195,7 @@ public class LazyEvaluator {
     final Thunk<Integer> p   = xs_.eval().head;
     Thunk<LList<Integer>> xs = xs_.eval().tail;
 
-    Fn<Integer, Boolean> pred = x -> gt(mod(x, p), Thunk.ready(0));
+    Thunk<Fn<Integer, Boolean>> pred = Thunk.ready(x -> gt(mod(x, p), Thunk.ready(0)));
 
     return LList.cons(p, sieve(filter(pred, xs))).eval();
   }); }
