503fcce10b69f4859b7f9c128a583eece60f0e08
Erik Allik <eallik@gmail.com>
2016-01-12 16:58:48 +0200

    refactor Thunk a little bit

--- Thunk.java
+++ Thunk.java
@@ -9,30 +9,35 @@
 public class Thunk<A> {
   /** Returns the value this Thunk wraps. */
   public final A eval() {
-    if (value != null) return value;
-    else { value = computation.compute(null); return value; }
+    if (value != null) {
+      return value;
+    } else {
+      value = computation.compute(null);
+      return value;
+    }
   }
 
+  /** Make a Thunk with an already computed value in it. */
+  public static <A> Thunk<A> ready(            A  x) { return new Thunk<A>(x); }
   /** Make a Thunk with a lazily executed computation in it. */
-  public Thunk(Computation<A> computation) {
+  public static <A> Thunk<A> lazy (Computation<A> x) { return new Thunk<A>(x); }
+
+  ///
+
+  private                   A  value = null;
+  private final Computation<A> computation;
+
+  private Thunk(Computation<A> computation) {
     this.computation = computation;
   }
-  /** Make a Thunk with an already computed value in it. */
-  public Thunk(A value)            {
+  private Thunk(A value)            {
     // FP *never* uses null pointers as real values, so we can safely use
     // nulls inside our low-level, (hopefully) hidden plumbing.
     assert(value != null);
 
     this.value = value;
+    this.computation = null;
   }
-
-  /** Convenience/readability alias for new Thunk<A>(value) */
-  public static <A> Thunk<A> ready(            A  x) { return new Thunk<A>(x); }
-  /** Convenience/readability alias for new Thunk<A>(computation) */
-  public static <A> Thunk<A> lazy (Computation<A> x) { return new Thunk<A>(x); }
-
-  private             A  value       = null;
-  private Computation<A> computation = null;
 
   /** a helper interface for Thunk.lazy that should not be used directly */
   public static interface Computation<A> { A compute(Void v); }
