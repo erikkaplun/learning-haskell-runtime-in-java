9187e1f22c818aaced171bd2b91e6a8b1632c2ff
Erik Allik <eallik@gmail.com>
2016-01-16 17:29:36 +0200

    add Bool and Tuple

---
+++ Bool.java
@@ -0,0 +1,19 @@
+public class Bool {
+  public static
+  Thunk<Fn<Boolean, Fn<Boolean, Boolean>>>
+  or() { return Thunk.ready(a -> Thunk.ready(b -> Thunk.lazy(__ -> 
+    a.eval().booleanValue() || b.eval().booleanValue()
+  ))); }
+
+  public static
+  Thunk<Fn<Boolean, Fn<Boolean, Boolean>>>
+  and() { return Thunk.ready(a -> Thunk.ready(b -> Thunk.lazy(__ -> 
+    a.eval().booleanValue() && b.eval().booleanValue()
+  ))); }
+
+  public static
+  Thunk<Fn<Boolean, Boolean>>
+  not() { return Thunk.ready(a -> Thunk.lazy(__ -> 
+    !a.eval().booleanValue()
+  )); }
+}
---
+++ Tuple.java
@@ -0,0 +1,42 @@
+public class Tuple<A, B> {
+  public static <A, B>
+  Thunk<Fn<A, Fn<B, Tuple<A, B>>>>
+  make() { return Thunk.ready(a -> Thunk.ready(b -> Thunk.lazy(__ ->
+    new Tuple<A, B>(a, b)
+  ))); }
+
+  /** Extracts the 1st element of a 2-tuple
+    *
+    * in Haskell, the type would be:
+    *
+    *     snd :: (a,b) -> a
+    */
+  public static <A, B>
+  Thunk<Fn<Tuple<A, B>, 
+           A>>
+  fst() { return Thunk.ready(
+     pair -> pair.eval().fst
+   ); }
+
+  /** Extracts the 2nd element of a 2-tuple
+    *
+    * in Haskell, the type would be: 
+    *
+    *     snd :: (a,b) -> b
+    */
+  public static <A, B>
+  Thunk<Fn<Tuple<A, B>,
+           B>>
+  snd() { return Thunk.ready(
+     pair -> pair.eval().snd
+   ); }
+
+  private Tuple(final Thunk<A> fst,
+                final Thunk<B> snd) {
+    this.fst = fst;
+    this.snd = snd;
+  }
+
+  private final Thunk<A> fst;
+  private final Thunk<B> snd;
+}
